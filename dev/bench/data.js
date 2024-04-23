7d3139cda257848e1c52e1cd8a15d999df656d0f
github-action-benchmark <github@users.noreply.github.com>
2024-04-23 09:13:00 +0000

    add Benchmark (benchmarkjs) benchmark result for 514dc68afc0169cc5c65ac3ed47ffe718617cfb5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712156615725,
+  "lastUpdate": 1713863580321,
   "repoUrl": "https://github.com/RafaelAPB/blockchain-integration-framework",
   "entries": {
     "Benchmark": [
@@ -36,6 +36,44 @@ window.BENCHMARK_DATA = {
             "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
             "value": 362,
             "range": "±1.17%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jerome.f.d.occiano@accenture.com",
+            "name": "jmoagacn",
+            "username": "jmoagacn"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "514dc68afc0169cc5c65ac3ed47ffe718617cfb5",
+          "message": "chore(connector-quorum): fix CVEs: CVE-2023-36665, CVE-2022-21190\n\nPrimary Changes\n----------------\n1. Modified the Dockerfile to use the updated versions\n   of the packages being used\n\nFixes #2865\n\nSigned-off-by: jmoagacn <jerome.f.d.occiano@accenture.com>",
+          "timestamp": "2024-04-18T10:08:29-07:00",
+          "tree_id": "368abe379b0937957d826403681e6c78a326ffd5",
+          "url": "https://github.com/RafaelAPB/blockchain-integration-framework/commit/514dc68afc0169cc5c65ac3ed47ffe718617cfb5"
+        },
+        "date": 1713863578382,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 580,
+            "range": "±1.70%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 361,
+            "range": "±1.24%",
             "unit": "ops/sec",
             "extra": "181 samples"
           }
